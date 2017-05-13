bbd7a6bf0e887573303878e036f20a90de963104
Rahul Maan <rahulmaan@Rahuls-MBP.lan>
2017-05-13 14:29:30 -0400

    First commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -1,23 +1,17 @@
-//
-//  ViewController.swift
-//  MyFirstApp
-//
-//  Created by Rahul Maan on 5/13/17.
-//  Copyright © 2017 Rahul Maan. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showMeText(_ sender: Any) {
+        label.text = "testing"
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
